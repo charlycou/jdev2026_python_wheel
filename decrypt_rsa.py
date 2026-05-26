37462492fb312c6ef5a4bac4998312689005bc48
Thomas BAUDIER <thomas.baudier@creatis.insa-lyon.fr>
2026-05-26 15:56:58 +0200

    Add the main function for the entry point

--- decrypt_rsa.py
+++ decrypt_rsa.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from helpers import (
+from .helpers import (
     break_rsa_with_primes,
     display_gui,
     rsa_decrypt,
@@ -10,30 +10,36 @@ from helpers import (
     show_text_gui,
 )
 
-# public key (n, e)
-n = 1067333190347  # 1292063 * 826069
-e = 17
 
-# If message is an integer
-# message = 123
-# c = rsa_encrypt(message, n, e)
-# encrypted message
-# c = 641618116209
-# plaintext = rsa_decrypt(c, d, n)
-# print("Decrypted message:", plaintext)
+def run_decrypt_rsa():
+    # public key (n, e)
+    n = 1067333190347  # 1292063 * 826069
+    e = 17
 
-# Get the message to encrypt and encrypt it
-message = "Hello"
-message = display_gui(message)
-c = rsa_encrypt_text(message, n, e)
-show_text_gui(str(c), title="Encrypted message", label="Encrypted message:")
+    # If message is an integer
+    # message = 123
+    # c = rsa_encrypt(message, n, e)
+    # encrypted message
+    # c = 641618116209
+    # plaintext = rsa_decrypt(c, d, n)
+    # print("Decrypted message:", plaintext)
 
-# Load the list of primes and break RSA to find d
-primes = np.load("primes.npz").tolist()
-d = break_rsa_with_primes(n, e, primes)
+    # Get the message to encrypt and encrypt it
+    message = "Hello"
+    message = display_gui(message)
+    c = rsa_encrypt_text(message, n, e)
+    show_text_gui(str(c), title="Encrypted message", label="Encrypted message:")
 
-# Decrypting the message
-decrypted_message = rsa_decrypt_text(c, d, n)
-show_text_gui(
-    str(decrypted_message), title="Decrypted message", label="Decrypted message:"
-)
+    # Load the list of primes and break RSA to find d
+    primes = np.load("primes.npz").tolist()
+    d = break_rsa_with_primes(n, e, primes)
+
+    # Decrypting the message
+    decrypted_message = rsa_decrypt_text(c, d, n)
+    show_text_gui(
+        str(decrypted_message), title="Decrypted message", label="Decrypted message:"
+    )
+
+
+def main():
+    run_decrypt_rsa()
